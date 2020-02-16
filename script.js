c86ec9f5d1c562565b483d9e5942d49ce42fe91e
mario <marioneta@mail.com>
2020-02-15 19:42:17 -0500

    Comments in js file

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 window.onload = function(){
 
+    //First link element
     var a1=document.querySelector('a:nth-child(1)');
 
+    //Whole nav and all elements whithin
     var nav1=document.querySelector('nav');
 
     a1.onmouseover=function(){    
